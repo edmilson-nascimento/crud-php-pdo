e224e64bd7988d9ec6a0543005c2d3bab1ef3078
Edmilson Nascimento <nascimento@abapconsulting.com.br>
2018-04-09 15:09:30 -0300

    Update and rename sql_script.txt to sql_script.sql

--- sql_script.sql
+++ sql_script.sql
@@ -1,3 +1,4 @@
+
 DROP TABLE IF EXISTS `exercicio`.`pessoa`;
 CREATE TABLE  `exercicio`.`pessoa` (
   `idpessoa` int(11) NOT NULL AUTO_INCREMENT,
@@ -5,3 +6,4 @@ CREATE TABLE  `exercicio`.`pessoa` (
   `email` varchar(45) NOT NULL,
   PRIMARY KEY (`idpessoa`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
